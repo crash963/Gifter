a72e5a546dfb7538f1fe077a09a1df76e9db13ee
crash963 <t-pokorny@seznam.cz>
2021-03-12 10:20:11 +0100

    working or react register/login

--- resources/js/homepage/index.js
+++ resources/js/homepage/index.js
@@ -2,5 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import Welcome from "./pages/welcome/welcome";
+import { Route, Router, Switch } from "react-router";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    <Router>
+        <Switch>
+            <Route path="/welcome" children={<Welcome />} />
+        </Switch>
+    </Router>,
+    document.getElementById("app")
+);
