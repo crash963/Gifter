4a1ac714b1103d1d33d7d36ab12b7b798858f92e
Tomas Nejedly <t.nejedly1@gmail.com>
2021-03-12 16:23:06 +0100

    styling of botsection

--- resources/js/homepage/pages/welcome/components/BotSection.jsx
+++ resources/js/homepage/pages/welcome/components/BotSection.jsx
@@ -11,12 +11,13 @@ function BotSection() {
             </div>
 
             <div className="promo__tile">
-                <div className="promo__tile--image">
-                    <img src="" alt="placeholder" />
-                </div>
                 <div className="promo__tile--text">
                     <h4>2 Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt dolores numquam, eius ipsam doloribus odio quos!</h4>
                 </div>
+                <div className="promo__tile--image">
+                    <img src="" alt="placeholder" />
+                </div>
+                
             </div>
 
             <div className="promo__tile">
