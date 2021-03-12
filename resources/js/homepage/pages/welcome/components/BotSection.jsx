ebc31c1206d6d31c0d3c843cc66b367ceb217f66
Tomas Nejedly <t.nejedly1@gmail.com>
2021-03-12 13:55:35 +0100

    adding styling to BotSection

--- resources/js/homepage/pages/welcome/components/BotSection.jsx
+++ resources/js/homepage/pages/welcome/components/BotSection.jsx
@@ -3,7 +3,7 @@ function BotSection() {
         <div className="welcome__page--bot--section">
             <div className="promo__tile">
                 <div className="promo__tile--image">
-                    <img src="" alt="placeholder" />
+                    <img src="../images/placeholder.png" alt="placeholder" />
                 </div>
                 <div className="promo__tile--text">
                     <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt dolores numquam, eius ipsam doloribus odio quos!</h4>
--- resources/views/react/app.blade.php
+++ resources/views/react/app.blade.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Document</title>
+    <link rel="stylesheet" href="../css/gifter.css">
 </head>
 <body>
     <div id="app"></div>
