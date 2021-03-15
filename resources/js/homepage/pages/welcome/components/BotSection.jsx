7deb34c29b478f12b8b8387bf3fe86cebb7e47bf
crash963 <t-pokorny@seznam.cz>
2021-03-15 14:22:16 +0100

    corrected botsection css

--- resources/js/homepage/pages/welcome/components/BotSection.jsx
+++ resources/js/homepage/pages/welcome/components/BotSection.jsx
@@ -2,34 +2,49 @@ function BotSection() {
     return (
         <div className="welcome__page--bot--section">
             <div className="promo__tile">
-                <div className="promo__tile--image">
-                    <img src="../images/placeholder.png" alt="placeholder" />
-                </div>
+                <img
+                    className="promo__tile--image"
+                    src="../images/placeholder.png"
+                    alt="placeholder"
+                />
                 <div className="promo__tile--text">
-                    <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt dolores numquam, eius ipsam doloribus odio quos!</h4>
+                    <h4>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Incidunt dolores numquam, eius ipsam doloribus odio
+                        quos!
+                    </h4>
                 </div>
             </div>
 
             <div className="promo__tile">
                 <div className="promo__tile--text">
-                    <h4>2 Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt dolores numquam, eius ipsam doloribus odio quos!</h4>
+                    <h4>
+                        2 Lorem ipsum dolor sit amet consectetur adipisicing
+                        elit. Incidunt dolores numquam, eius ipsam doloribus
+                        odio quos!
+                    </h4>
                 </div>
-                <div className="promo__tile--image">
-                    <img src="../images/placeholder.png" alt="placeholder" />
-                </div>
-                
+                <img
+                    className="promo__tile--image"
+                    src="../images/placeholder.png"
+                    alt="placeholder"
+                />
             </div>
 
             <div className="promo__tile">
-                <div className="promo__tile--image">
-
-                    <img src="../images/placeholder.png" alt="placeholder" />
-                </div>
+                <img
+                    className="promo__tile--image"
+                    src="../images/placeholder.png"
+                    alt="placeholder"
+                />
                 <div className="promo__tile--text">
-                    <h4>3 Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt dolores numquam, eius ipsam doloribus odio quos!</h4>
+                    <h4>
+                        3 Lorem ipsum dolor sit amet consectetur adipisicing
+                        elit. Incidunt dolores numquam, eius ipsam doloribus
+                        odio quos!
+                    </h4>
                 </div>
             </div>
-            
         </div>
     );
 }
