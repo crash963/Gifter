7ddfe84e7664bc69fe63feca5a046bf3b0cfe904
crash963 <t-pokorny@seznam.cz>
2021-03-16 10:39:00 +0100

    trying to use reactContext

--- resources/js/homepage/pages/profile/Profile.jsx
+++ resources/js/homepage/pages/profile/Profile.jsx
@@ -1,5 +1,32 @@
+import React, { useState } from "react";
+import { useEffect } from "react";
+import UserBox from "./components/UserBox";
+import { createContext } from "react";
+
 function Profile() {
-    return <section className="profile">hello</section>;
+    const CurrentUserContext = React.createContext(null);
+    const [currentUser, setCurrentUser] = useState(null);
+
+    async function fetchCurrentUser() {
+        const response = await fetch("/api/current-user");
+        const data = await response.json();
+        console.log(data);
+        setCurrentUser(data);
+    }
+
+    useEffect(() => {
+        fetchCurrentUser();
+    }, []);
+
+    return (
+        <CurrentUserContext.Provider value={currentUser}>
+            <section className="profile">
+                <div className="box__container">
+                    <UserBox />
+                </div>
+            </section>
+        </CurrentUserContext.Provider>
+    );
 }
 
 export default Profile;
