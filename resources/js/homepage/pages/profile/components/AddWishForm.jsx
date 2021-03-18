203ce0f6e42cf5b33c66440ee50f2c118a07033f
crash963 <t-pokorny@seznam.cz>
2021-03-18 15:04:50 +0100

    still working on wish form"

--- resources/js/homepage/pages/profile/components/AddWishForm.jsx
+++ resources/js/homepage/pages/profile/components/AddWishForm.jsx
@@ -38,10 +38,15 @@ function AddWishForm() {
     return (
         <>
             <form action="post">
+                <label htmlFor="name"></label>
                 <input type="text" name="name" onChange={handleChange} />
+                <label htmlFor="link"></label>
                 <input type="url" name="link" onChange={handleChange} />
-                <input type="text" name="description" onChange={handleChange} />
+                <label htmlFor="description"></label>
+                <textarea name="description" cols="30" rows="10"></textarea>
+                <label htmlFor="photo"></label>
                 <input type="text" name="photo" onChange={handleChange} />
+                <label htmlFor="resolve_date"></label>
                 <select name="resolve_date" onChange={handleChange}>
                     <option value={currentUser.birthday.slice(5)}>
                         Birthday
@@ -50,11 +55,14 @@ function AddWishForm() {
                     <option value={null}>No Specific Date</option>
                 </select>
                 {resolve_date === "other_date" && (
-                    <input
-                        type="date"
-                        name="resolve_date"
-                        onChange={handleChange}
-                    />
+                    <>
+                        <label htmlFor="date"></label>
+                        <input
+                            type="date"
+                            name="resolve_date"
+                            onChange={handleChange}
+                        />
+                    </>
                 )}
                 <input type="submit" value="Add Wish" />
             </form>
