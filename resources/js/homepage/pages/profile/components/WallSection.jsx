98313a87dd0dc38d054db85904553776c64e7186
crash963 <t-pokorny@seznam.cz>
2021-03-19 16:55:27 +0100

    change

--- resources/js/homepage/pages/profile/components/WallSection.jsx
+++ resources/js/homepage/pages/profile/components/WallSection.jsx
@@ -1,5 +1,16 @@
+import { useState } from "react";
+
 function WallSection() {
-    return <div>wall</div>;
+    const [wishes, setWishes] = useState([]);
+
+    async function fetchFriendsWishes() {
+        const response = await fetch("/user/{user_id}/friends-wishes");
+        const data = await response.json();
+
+        setWishes(data);
+    }
+
+    return <div className=""></div>;
 }
 
 export default WallSection;
