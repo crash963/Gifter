87dc1d6941bd4aa6602542fcd871f8757cd108d4
crash963 <t-pokorny@seznam.cz>
2021-03-22 13:13:02 +0100

    working on comments

--- resources/js/homepage/pages/profile/components/WishBox.jsx
+++ resources/js/homepage/pages/profile/components/WishBox.jsx
@@ -2,14 +2,26 @@ import { useContext, useEffect, useState } from "react";
 import { CurrentUserContext } from "../Profile.jsx";
 
 function WishBox(props) {
+    const currentUser = useContext(CurrentUserContext);
     const wish = props.wish;
     const [author, setAuthor] = useState(null);
+    const [isUserAuthor, setIsUserAuthor] = useState(true);
+    const [isBoxClicked, setIsBoxClicked] = useState(false);
 
     async function fetchAuthor() {
         const response = await fetch(`/api/user/${wish.user_id}`);
         const data = await response.json();
 
+        setIsUserAuthor(data.id === currentUser.id);
         setAuthor(data);
+    }
+
+    function handleClick() {
+        if (isBoxClicked === false) {
+            setIsBoxClicked(true);
+        } else {
+            setIsBoxClicked(false);
+        }
     }
 
     useEffect(() => {
@@ -19,35 +31,68 @@ function WishBox(props) {
     return (
         <>
             {author && (
-                <div className="wish__box box">
+                <div
+                    className={`wish__box box ${
+                        isBoxClicked && "wish__box--clicked"
+                    } ${!isUserAuthor && "wish__box__with__comments"}`}
+                >
                     <div className="wish__user__info">
-                        <img
-                            src={
-                                author.photo
-                                    ? `../images/${author.photo}`
-                                    : `../images/profile_pictures/placeholder-profile-pic.jpg`
-                            }
-                            alt={`${author.nickname}'s Profile Picture`}
-                        />
+                        {
+                            <img
+                                src={
+                                    author.photo
+                                        ? `../images/${author.photo}`
+                                        : `../images/profile_pictures/placeholder-profile-pic.jpg`
+                                }
+                                alt={`${author.nickname}'s Profile Picture`}
+                            />
+                        }
                         <p>
                             {props.isNameSet
                                 ? `${author.first_name} ${author.last_name}`
                                 : author.nickname}
                         </p>
                     </div>
-                    <img
-                        src={
-                            wish.photo
-                                ? `../images/${wish.photo}`
-                                : `../images/test-wish-photo.jpg`
-                        }
-                        alt="wish-img"
-                    />
+                    <button onClick={handleClick}>
+                        {isBoxClicked ? "hide" : "detail"}
+                    </button>
+                    {!isBoxClicked && (
+                        <img
+                            src={
+                                wish.photo
+                                    ? `../images/${wish.photo}`
+                                    : `../images/test-wish-photo.jpg`
+                            }
+                            alt="wish-img"
+                        />
+                    )}
                     <div className="wish__box--wish">
                         <p>
-                            {wish.name} {""}
+                            {wish.name}
                             {wish.link && <a href={wish.link}>Buy NOW</a>}
                         </p>
+                        {isBoxClicked && (
+                            <>
+                                <p>{wish.description && wish.description}</p>
+
+                                {wish.resolve_date && (
+                                    <p>date: {wish.resolve_date}</p>
+                                )}
+                            </>
+                        )}
+                        {!isUserAuthor && isBoxClicked && (
+                            <div className="comments__container">
+                                <form>
+                                    <textarea
+                                        name="text"
+                                        cols="30"
+                                        rows="8"
+                                        placeholder="Your comment"
+                                    ></textarea>
+                                    <input type="submit" value="comment" />
+                                </form>
+                            </div>
+                        )}
                     </div>
                 </div>
             )}
