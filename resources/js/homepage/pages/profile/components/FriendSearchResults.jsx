e91d24c245e8ce10b8ce4e1ff5d2ba01eae98159
Teo15sk <teo15sk@gmail.com>
2021-03-22 15:13:31 +0100

    friends search results box

--- resources/js/homepage/pages/profile/components/FriendSearchResults.jsx
+++ resources/js/homepage/pages/profile/components/FriendSearchResults.jsx
@@ -1,5 +1,26 @@
-function FriendSearchResults() {
-    return <h5>Search results:</h5>;
+function FriendSearchResults(props) {
+    console.log(props.friend);
+    const { photo, first_name, last_name, nickname } = props.friend;
+
+    return (
+        <div className="user__box friend-search-result">
+            <div className="friend-search-result__heading">
+                <img
+                    src={
+                        photo
+                            ? `/images/${photo}`
+                            : "/images/profile_pictures/placeholder-profile-pic.jpg"
+                    }
+                    alt="user-img"
+                    className="friend-search-result__photo"
+                />
+                <p className="friend-search-result__nickname">{nickname}</p>
+            </div>
+            <p lassName="friend-search-result__name">
+                {first_name && last_name && `${first_name} ${last_name}`}
+            </p>
+        </div>
+    );
 }
 
 export default FriendSearchResults;
