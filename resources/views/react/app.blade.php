9079ce6132326426a8c03f0cd520fdd9e5920cb5
Tomas Nejedly <t.nejedly1@gmail.com>
2021-03-24 11:50:17 +0100

    font adjustment

--- resources/views/react/app.blade.php
+++ resources/views/react/app.blade.php
@@ -8,7 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Document</title>
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Monoton&family=Lato:ital,wght@0,400;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../css/gifter.css">
 </head>
 <body>
