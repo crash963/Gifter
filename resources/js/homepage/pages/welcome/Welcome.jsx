82773d3de9e85e0b3b0833b5cbe33dc8e6acc621
crash963 <t-pokorny@seznam.cz>
2021-03-26 13:24:32 +0100

    added redirect from welcome page to profile when you are logged in

--- resources/js/homepage/pages/welcome/Welcome.jsx
+++ resources/js/homepage/pages/welcome/Welcome.jsx
@@ -1,13 +1,21 @@
 import BotSection from "./components/BotSection";
 import TopSection from "./components/TopSection";
 import ReviewSlider from "./components/ReviewSlider";
+import { useEffect } from "react";
 
 function Welcome() {
     async function fetchCurrentUser() {
         const response = await fetch("/api/current-user");
-        if (response.status == 200) {
+        const data = await response.json();
+        console.log(data);
+        if (data) {
+            window.location.href = "/profile";
         }
     }
+
+    useEffect(() => {
+        fetchCurrentUser();
+    }, []);
 
     return (
         <section className="welcome__page">
